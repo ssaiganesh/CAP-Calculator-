3f25605e4ab8140460aef226e507514cca521ec4
Sai Ganesh <shankarsaiganesh@gmail.com>
2020-05-14 20:33:18 +0800

    Create Cap Calculator.py

---
+++ Cap Calculator.py
@@ -0,0 +1,39 @@
+
+##This can be changed to dictionary. 
+
+grades_cap = {"A+": 5.0 , "A": 5.0, "A-": 4.5, 'B+':4.0, 'B':3.5,'B-': 3.0, 'C+': 2.5, 'C': 2.0, 'D+': 1.5, 'D': 1.0, 'F':0 }
+
+def freshie():
+    freshie_check = input("Is this your first sem? ")
+    if freshie_check == "N":
+        current_cap = float(input("What is your current cap? "))
+        completed_mc = int(input("How many Modular Credits have been completed? "))
+    else:
+        current_cap = 0
+        completed_mc = 0
+    return current_cap, completed_mc
+
+current_cap, completed_mc = freshie()
+
+def new_sem():
+    num_mods = int(input("How many mods did you complete? "))
+    sap = 0
+    new_mc = 0
+    for i in range(1,(num_mods+1)):
+        new_mc = int(input("What is the number of MCs for mod",i"? "))
+        new_grade = input("What is the grade? ")
+        sap += grades_cap[new_grade.upper()] * new_mc
+        new_mc += new_mc
+    return sap, new_mc
+
+sap, new_mc = new_sem()
+
+def main():
+    updated_cap = (current_cap*completed_mc + sap)/(completed_mc + new_mc)
+    print(updated_cap)
+
+
+if __name__ == '__main__': main()
+
+
+
